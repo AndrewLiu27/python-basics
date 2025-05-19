3f2b675c2772ba2dd2827412984faf8f56240aa4
AndrewLiu27 <liuzy200127@gmail.com>
2025-05-19 00:48:58 -0500

    first commit for python basic file

---
+++ basic.py
@@ -0,0 +1,24 @@
+# Comment
+
+# Create and use variables
+"""
+x = 3
+y = 4
+print(x)
+
+m = 0
+for i in [1,2,3]:
+    m += 1
+    print(m)
+
+if x <= 3:
+    x += 1
+else:
+    x = y
+
+print(x)
+print(y)
+"""
+
+a = input("Enter your first number:")
+print(a)
